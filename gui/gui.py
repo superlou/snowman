f5018bae0093a8206b73e46ab1a0d0c2fdeca281
superlou <lousimons@gmail.com>
2016-08-21 18:59:57 -0400

    Busted GUI

---
+++ gui/gui.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class PongGame(Widget):
+    pass
+
+class PongApp(App):
+    def build(self):
+        return PongGame()
+
+
+if __name__ == '__main__':
+    PongApp().run()
